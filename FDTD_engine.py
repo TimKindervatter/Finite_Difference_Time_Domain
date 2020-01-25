c2a9d781875cac53683cfd648b535fe256d57623
Tim <kindert1@tcnj.edu>
2020-01-25 13:46:06 -0500

    Fixed Dirichlet boundary conditions by moving boundary updates prior to field update loops

--- FDTD_engine.py
+++ FDTD_engine.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 c = 299792458   # Speed of light in m/s
 steps = 200     # Number of iterations
 
-Nres = 10       # Number of points to resolve a wave with
+Nres = 1       # Number of points to resolve a wave with
 dz = 1/Nres
-Nz = round(10/dz)        # Number of grid points along the z-axis
+Nz = 100        # Number of grid points along the z-axis
 
 
 dmin = 1        # Size of smallest feature in geomtetry
@@ -29,7 +29,7 @@ nmax = 1.0      # Maximum refractive index
 # dz = dc/N
 
 zmin = 0
-zmax = 10
+zmax = Nz*dz
 z = np.linspace(zmin, zmax, Nz)
 
 epsilon_r = np.ones(Nz)
@@ -41,9 +41,9 @@ dt = nbc*dz/(2*c)
 
 #  Pulse parameters
 # _lambda_min = min(_lambda)
-# fmax = c/(_lambda_min*nmax)
+fmax = 0.1
 nzsrc = Nz//2
-tau = 0.5/dz
+tau = 0.5/fmax
 t0 = 5*tau
 g = lambda t: np.exp(-((t - t0)/tau)**2)
 A = np.sqrt(epsilon_r[nzsrc]/mu_r[nzsrc])
@@ -66,32 +66,33 @@ e2 = 0
 e1 = 0
 
 plt.figure(1)
-axes = plt.gca()
-axes.set_xlim([zmin, zmax])
-axes.set_ylim([-1, 1])
 
 for T in range(steps):
+    # h3 = h2
+    h2 = h1
+    h1 = Hx[0]
     for nz in range(Nz-1):
         Hx[nz] = Hx[nz] + mHx[nz]*(Ey[nz + 1] - Ey[nz])/dz
     Hx[Nz-1] = Hx[Nz - 1] + mHx[Nz-1]*(e2 - Ey[Nz - 1])/dz
-    h3 = h2
-    h2 = h1
-    h1 = Hx[0]
 
-    # Hx[nzsrc-1] = Hx[nzsrc-1] - (mHx[nzsrc-1]/dz)*Eysrc(T)
+    Hx[nzsrc-1] = Hx[nzsrc-1] - (mHx[nzsrc-1]/dz)*Eysrc(T)
 
+    # e3 = e2
+    e2 = e1
+    e1 = Ey[Nz - 1]
     Ey[0] = Ey[0] + mEy[0]*(Hx[0] - h2)/dz
     for nz in range(1, Nz):
         Ey[nz] = Ey[nz] + mEy[nz]*(Hx[nz] - Hx[nz - 1])/dz
-    e3 = e2
-    e2 = e1
-    e1 = Ey[Nz - 1]
 
-    # Ey[nzsrc] = Ey[nzsrc] - (mEy[nzsrc]/dz)*Hxsrc(T)
-    Ey[nzsrc] = Ey[nzsrc] + g(T)
+
+    Ey[nzsrc] = Ey[nzsrc] - (mEy[nzsrc]/dz)*Hxsrc(T)
+    # Ey[nzsrc] = Ey[nzsrc] + g(T)
 
     plt.plot(z, Ey)
     plt.plot(z, Hx)
+    axes = plt.gca()
+    axes.set_xlim([zmin, zmax])
+    axes.set_ylim([-1.1, 1.1])
     plt.pause(1/60)
     plt.cla()
 
